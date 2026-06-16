6355b3b8fa7e9e1f4b6eb7a54af479e280f222da
Julien Herr <julien@herr.fr>
2026-06-16 08:27:12 +0200

    feat: add Spanish RIPREE registry support

--- src/connectors/es-ripree/index.ts
+++ src/connectors/es-ripree/index.ts
@@ -1,5 +1,7 @@
-import { NotImplementedError, type RegistryConnector } from "../connector.js";
+import type { RegistryConnector } from "../connector.js";
+import { RipreeConnector } from "./ripree.connector.js";
+import type { RipreeXmlRow } from "./ripree.types.js";
 
-export function createRipreeConnector(): RegistryConnector {
-  throw new NotImplementedError("es-ripree");
+export function createRipreeConnector(): RegistryConnector<RipreeXmlRow> {
+  return new RipreeConnector();
 }
